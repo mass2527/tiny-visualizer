df783cc57687f8c3e6eaca3d8c95a978fe68e563
Dongho Kim <dongre2527@gmail.com>
2023-01-31 16:29:14 +0900

    Implement draw

---
+++ src/machines/canvasMachine.typegen.ts
@@ -0,0 +1,28 @@
+// This file was automatically generated. Edits will be overwritten
+
+export interface Typegen0 {
+  "@@xstate/typegen": true;
+  internalEvents: {
+    "xstate.init": { type: "xstate.init" };
+  };
+  invokeSrcNameMap: {};
+  missingImplementations: {
+    actions: "draw" | "generateDraw";
+    delays: never;
+    guards: never;
+    services: never;
+  };
+  eventsCausingActions: {
+    addElement: "DRAW_START";
+    deleteElement: "ELEMENT.DELETE";
+    draw: "ELEMENT.DELETE" | "ELEMENT.UPDATE" | "ELEMENT.UPDATE_END";
+    generateDraw: "ELEMENT.UPDATE" | "ELEMENT.UPDATE_END";
+    stopElement: "ELEMENT.DELETE";
+    updateElement: "ELEMENT.UPDATE" | "ELEMENT.UPDATE_END";
+  };
+  eventsCausingDelays: {};
+  eventsCausingGuards: {};
+  eventsCausingServices: {};
+  matchesStates: "drawing" | "idle";
+  tags: never;
+}
