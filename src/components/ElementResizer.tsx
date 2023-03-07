91de29016eb4ed75d1953aac05b3afa4b1e8f93c
Dongho Kim <dongre2527@gmail.com>
2023-03-08 00:35:49 +0900

    Colocate createElementVirtualPoints function

--- src/components/ElementResizer.tsx
+++ src/components/ElementResizer.tsx
@@ -3,8 +3,17 @@ import {
   VisualizerElement,
   VisualizerMachineContext,
 } from "../machines/visualizerMachine";
-import { createElementVirtualPoints } from "../utils";
-import VirtualPoint from "./VirtualPoint";
+import {
+  calculateElementAbsolutePoint,
+  calculateElementViewportSize,
+  convertToViewportPoint,
+  isTextElement,
+} from "../utils";
+import VirtualPoint, {
+  VirtualPointProps,
+  VIRTUAL_POINT_HEIGHT,
+  VIRTUAL_POINT_WIDTH,
+} from "./VirtualPoint";
 
 type HorizontalDirection = "left" | "right";
 type VerticalDirection = "up" | "down";
@@ -14,6 +23,122 @@ export type Direction =
   | HorizontalDirection
   | VerticalDirection
   | DiagonalDirection;
+
+const createElementVirtualPoints = ({
+  element,
+  devicePixelRatio,
+  origin,
+  zoom,
+  virtualPointSize = {
+    width: VIRTUAL_POINT_WIDTH,
+    height: VIRTUAL_POINT_HEIGHT,
+  },
+}: {
+  element: VisualizerElement;
+  devicePixelRatio: number;
+  origin: VisualizerMachineContext["origin"];
+  zoom: VisualizerMachineContext["zoom"];
+  virtualPointSize?: Required<Pick<VirtualPointProps, "width" | "height">>;
+}) => {
+  const absolutePoint = calculateElementAbsolutePoint(element);
+  const elementViewportPoint = convertToViewportPoint({
+    canvasPoint: {
+      x: absolutePoint.minX,
+      y: absolutePoint.minY,
+    },
+    devicePixelRatio,
+    origin,
+    zoom,
+  });
+  const elementViewportSize = calculateElementViewportSize({
+    element,
+    devicePixelRatio,
+    zoom,
+  });
+
+  const virtualPoints: ({
+    direction: Direction;
+  } & Required<
+    Pick<VirtualPointProps, "top" | "left" | "width" | "height">
+  >)[] = [
+    {
+      direction: "up-left",
+      left: elementViewportPoint.x,
+      top: elementViewportPoint.y,
+      width: virtualPointSize.width,
+      height: virtualPointSize.height,
+    },
+    {
+      direction: "up-right",
+      left: elementViewportPoint.x + elementViewportSize.width,
+      top: elementViewportPoint.y,
+      width: virtualPointSize.width,
+      height: virtualPointSize.height,
+    },
+    {
+      direction: "down-left",
+      left: elementViewportPoint.x,
+      top: elementViewportPoint.y + elementViewportSize.height,
+      width: virtualPointSize.width,
+      height: virtualPointSize.height,
+    },
+    {
+      direction: "down-right",
+      left: elementViewportPoint.x + elementViewportSize.width,
+      top: elementViewportPoint.y + elementViewportSize.height,
+      width: virtualPointSize.width,
+      height: virtualPointSize.height,
+    },
+  ];
+
+  if (isTextElement(element)) {
+    return virtualPoints;
+  }
+
+  const haveEnoughWidth =
+    Math.abs(elementViewportSize.width) >= 2 * (3 * virtualPointSize.width);
+  if (haveEnoughWidth) {
+    virtualPoints.push(
+      {
+        direction: "up",
+        left: elementViewportPoint.x + elementViewportSize.width / 2,
+        top: elementViewportPoint.y,
+        width: virtualPointSize.width,
+        height: virtualPointSize.height,
+      },
+      {
+        direction: "down",
+        left: elementViewportPoint.x + elementViewportSize.width / 2,
+        top: elementViewportPoint.y + elementViewportSize.height,
+        width: virtualPointSize.width,
+        height: virtualPointSize.height,
+      }
+    );
+  }
+
+  const haveEnoughHeight =
+    Math.abs(elementViewportSize.height) >= 2 * (3 * virtualPointSize.height);
+  if (haveEnoughHeight) {
+    virtualPoints.push(
+      {
+        direction: "left",
+        left: elementViewportPoint.x,
+        top: elementViewportPoint.y + elementViewportSize.height / 2,
+        width: virtualPointSize.width,
+        height: virtualPointSize.height,
+      },
+      {
+        direction: "right",
+        left: elementViewportPoint.x + elementViewportSize.width,
+        top: elementViewportPoint.y + elementViewportSize.height / 2,
+        width: virtualPointSize.width,
+        height: virtualPointSize.height,
+      }
+    );
+  }
+
+  return virtualPoints;
+};
 
 function ElementResizer({
   element,
@@ -50,6 +175,8 @@ function ElementResizer({
               event.preventDefault();
               onMouseDown(event, virtualPoint.direction);
             }}
+            width={virtualPoint.width}
+            height={virtualPoint.height}
           />
         );
       })}
