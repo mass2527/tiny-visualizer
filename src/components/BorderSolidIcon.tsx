77c2dd1b971f92384c41c319a954e3b7008cd298
Dongho Kim <dongre2527@gmail.com>
2023-04-09 22:43:17 -0400

    Add BorderSolidIcon

--- src/components/BorderSolidIcon.tsx
+++ src/components/BorderSolidIcon.tsx
@@ -1,4 +1,4 @@
-function MinusIcon() {
+function BorderSolidIcon() {
   return (
     <svg
       width="16"
@@ -8,7 +8,7 @@ function MinusIcon() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M2.25 7.5C2.25 7.22386 2.47386 7 2.75 7H12.25C12.5261 7 12.75 7.22386 12.75 7.5C12.75 7.77614 12.5261 8 12.25 8H2.75C2.47386 8 2.25 7.77614 2.25 7.5Z"
+        d="M1.25 7.5C1.25 7.22386 1.47386 7 1.75 7H13.25C13.5261 7 13.75 7.22386 13.75 7.5C13.75 7.77614 13.5261 8 13.25 8H1.75C1.47386 8 1.25 7.77614 1.25 7.5Z"
         fill="currentColor"
         fillRule="evenodd"
         clipRule="evenodd"
@@ -17,4 +17,4 @@ function MinusIcon() {
   );
 }
 
-export default MinusIcon;
+export default BorderSolidIcon;
