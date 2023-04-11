5bf4eb8421e890bbebd3660109886c086ba6c3be
Dongho Kim <dongre2527@gmail.com>
2023-04-11 02:33:00 -0400

    Create centeredSquare style constant

--- src/components/RadioCardGroup.tsx
+++ src/components/RadioCardGroup.tsx
@@ -1,6 +1,7 @@
 import * as RadioGroup from "@radix-ui/react-radio-group";
 import { RadioGroupItemProps } from "@radix-ui/react-radio-group";
 import { ReactNode } from "react";
+import { centeredSquare } from "../utils/style";
 
 const RadioCardGroupRoot = RadioGroup.Root;
 
@@ -20,7 +21,7 @@ function RadioCardGroupItem({
   return (
     <label aria-label={label} className="pointer-events-auto">
       <RadioGroup.Item
-        className={`flex items-center justify-center w-8 h-8 rounded-lg text-gray11 ${className}
+        className={`${centeredSquare} rounded-lg text-gray11 ${className}
         ${checked && "bg-blue9 text-white"}
         ${!checked && "hover:bg-blue10 hover:text-white"}`}
         value={value}
---
+++ src/utils/style.ts
@@ -0,0 +1,1 @@
+export const centeredSquare = "flex justify-center items-center p-2";
