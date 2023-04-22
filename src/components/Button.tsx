42c3f8e63c7f9921d5f47744bfaa21bda754a8f2
Dongho Kim <dongre2527@gmail.com>
2023-04-23 00:22:35 +0200

    add button variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,34 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 import { centeredSquare } from "../utils/style";
 
-function Button({
-  className,
-  onClick,
-  disabled,
-  children,
-}: {
+type ButtonProps = {
   className?: string;
+  type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
   onClick: VoidFunction;
   disabled?: boolean;
   children: ReactNode;
-}) {
+  variant?: "primary" | "secondary";
+};
+
+export function Button({
+  className,
+  variant = "secondary",
+  onClick,
+  type = "button",
+  disabled,
+  children,
+}: ButtonProps) {
+  const baseClassName = (
+    {
+      primary: "bg-gray12 text-gray11 hover:text-white hover:bg-blue9",
+      secondary: "bg-black text-white disabled:text-gray11 text-xs",
+    } satisfies Record<NonNullable<ButtonProps["variant"]>, string>
+  )[variant];
+
   return (
     <button
-      type="button"
-      className={`${centeredSquare} bg-black text-white disabled:text-gray11 text-xs ${className}`}
+      type={type}
+      className={`${centeredSquare} rounded-lg ${baseClassName} ${className}`}
       onClick={onClick}
       disabled={disabled}
     >
@@ -23,5 +36,3 @@ function Button({
     </button>
   );
 }
-
-export default Button;
